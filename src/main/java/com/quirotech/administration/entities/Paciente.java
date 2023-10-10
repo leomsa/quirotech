6d08eec714d0660adb05c66a9583d3918706f788
leomsa <leo.akimim@gmail.com>
2023-10-10 16:36:07 -0300

    Changed Paciente Getters and Setters to @Data

--- src/main/java/com/quirotech/administration/entities/Paciente.java
+++ src/main/java/com/quirotech/administration/entities/Paciente.java
@@ -4,28 +4,30 @@ package com.quirotech.administration.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 
-@Getter
-@Setter
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 
-@Table(name="paciente")
-@Entity(name="paciente")
+@Table(name = "paciente")
+@Entity(name = "paciente")
 public class Paciente implements Serializable {
 
     @Id
     public String cpf;
 
-   private String nome;
-   private LocalDate dataNascimento;
-   private String sexo;
-   private String senha;
-   private String pais;
+    private String nome;
+    private LocalDate dataNascimento;
+    private String sexo;
+    private String senha;
+    private String pais;
     //private List<Contato> contatos;
 }
